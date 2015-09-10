f4a6a08b8b58d7aa32ee17e6c7426665a70ba24d
Piotr Polak <pepis@closbrothers.pl>
2015-09-10 15:51:57 +0200

    Controller code review

--- src/controller/IMClientController.java
+++ src/controller/IMClientController.java
@@ -14,7 +14,7 @@ import model.SyncObjectSender;
 import view.client.FileTransferDialog;
 
 /**
- * Client controller
+ * Client controller, to be used as a Singletone
  *
  * Creates GUI, listener, contains interpret method for incoming messages,
  * actions of gui
@@ -29,19 +29,26 @@ public class IMClientController extends AbstractController {
      */
     protected static IMClientController instance = null;
 
+    /**
+     * Defines possible application states
+     */
+    private enum ApplicationStages {
+
+        LOGIN_FORM, SIGNIN_IN_PROGRESS, SIGNED_IN
+    };
+
     private MessageListener messageListener;
     private UserList userList;
     private ClientWindow view;
     private String serverIp;
-
-    private enum ApplicationStages {
-        LOGIN, SIGNIN, SIGNED_IN
-    };
-    
     private ApplicationStages applicationStage;
     private User user;
 
-
+    /**
+     * Singletone getInstance method
+     *
+     * @return
+     */
     public static IMClientController getInstance() {
         if (IMClientController.instance == null) {
             IMClientController.instance = new IMClientController();
@@ -50,96 +57,138 @@ public class IMClientController extends AbstractController {
         return IMClientController.instance;
     }
 
+    /**
+     * Private controller for the singletone
+     */
     private IMClientController() {
         this.view = new ClientWindow();
         this.println("Initializing IMClientController\n");
     }
 
+    /**
+     * The app starter method
+     *
+     * @param args
+     */
     public static void main(String args[]) {
+        // Initializes the instance of the client controller
         IMClientController.getInstance();
     }
 
     /**
      * Method for handeling incoming messages
      *
-     * @param mx
+     * @param m
+     * @param socket
      */
     public void handleIncomingMessage(Object m, Socket socket) {
+
+        // Logging
         this.println("Handeling incoming request");
 
+        // Checking the type of the message
         if (m instanceof ServerSignInSuccessMessage) {
-            if (this.applicationStage != ApplicationStages.SIGNIN) {
+
+            // This is valid only when the user is SIGNIN_IN_PROGRESS
+            if (this.applicationStage != ApplicationStages.SIGNIN_IN_PROGRESS) {
                 return;
             }
-
+            // Logging
             this.println("Received ServerSignInSuccessMessage");
-
+            // Changing the view state
             this.view.signedIn();
-
+            // Changing state of the controller
             this.applicationStage = ApplicationStages.SIGNED_IN;
+            // Requesting a userlist
             this.sendObjectToServer(new RefreshUserlistRequestMessage());
+
         } else if (m instanceof ServerSignInFailureMessage) {
-            if (this.applicationStage != ApplicationStages.SIGNIN) {
+
+            // This is valid only when the user is SIGNIN_IN_PROGRESS
+            if (this.applicationStage != ApplicationStages.SIGNIN_IN_PROGRESS) {
                 return;
             }
-
+            // Logging
             this.println("Received ServerSignInFailureMessage");
-
+            // Changing the view state
             this.view.signinRollback("Username already taken.");
+            // Stopping the message listener
             this.messageListener.stopListener();
+            // Changing state of the controller
+            this.applicationStage = ApplicationStages.LOGIN_FORM;
 
-            this.applicationStage = ApplicationStages.LOGIN;
-            //this.sendObjectToServer( new RefreshUserlistRequestMessage() );
         } else if (m instanceof PingRequestMessage) {
-            this.println("Received PingRequestMessage");
-            new AsyncObjectSender(new PingResponseMessage(), socket, this, false);
-        } else if (m instanceof RefreshUserlistResponseMessage) {
-            this.println("Received RefreshUserlistResponseMessage");
 
+            // Logging
+            this.println("Received PingRequestMessage");
+            // Responding for the ping message
+            new AsyncObjectSender(new PingResponseMessage(), socket, this, false);
+
+        } else if (m instanceof RefreshUserlistResponseMessage) {
+
+            // Logging
+            this.println("Received RefreshUserlistResponseMessage");
+            // Closing the incoming socket
             try {
                 socket.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
+            // Extracting the user list
             this.userList = ((RefreshUserlistResponseMessage) m).getUserlist();
-
+            // Logging
             this.println(user.getUsername() + " Userlist received, size: " + this.userList.size());
-
+            // Redrawing the userlist
             this.view.redrawUserlist();
+
         } else if (m instanceof TextMessage) {
+
+            // Logging
             this.println("Received TextMessage");
 
-            TextMessage tm = (TextMessage) m;
-            this.view.displayConversationWindow(tm.getFrom(), tm.getBody());
+            // Closing the incoming socket
             try {
                 socket.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+            // Extracting the message
+            TextMessage tm = (TextMessage) m;
+            // Displaying GUI window with the conversation
+            this.view.displayConversationWindow(tm.getFrom(), tm.getBody());
+
         } else if (m instanceof FileTransferRequestMessage) {
+
+            // Logging
             this.println("Received FileTransferRequestMessage");
 
+            // Starting a file receive thread
             new AsyncFileReader(socket, ((FileTransferRequestMessage) m).getFileName(), ((FileTransferRequestMessage) m).getFileSize(), new FileTransferDialog(((FileTransferRequestMessage) m).getFileName(), ((FileTransferRequestMessage) m).getFileSize()));
+
         } else {
-            this.println("Wrong request");
+            // Logging
+            this.println("Wrong request, unknown message type");
         }
     }
 
     /**
      * Notify method is called from the ParallelObjectSender on failure
+     *
+     * @param e
      */
     public void notify(Exception e) {
-        if (this.applicationStage == ApplicationStages.SIGNIN) {
+
+        // TODO Make it deterministic
+        if (this.applicationStage == ApplicationStages.SIGNIN_IN_PROGRESS) {
             this.view.signinRollback("Server not listening.");
             this.messageListener.stopListener();
-
         }
-        e.printStackTrace();
+        //e.printStackTrace();
     }
 
     /**
-     * Returns userlist
+     * Returns the userlist
      *
      * @return
      */
@@ -148,7 +197,7 @@ public class IMClientController extends AbstractController {
     }
 
     /**
-     * Returns gui window
+     * Returns GUI window
      *
      * @return
      */
@@ -156,6 +205,11 @@ public class IMClientController extends AbstractController {
         return this.view;
     }
 
+    /**
+     * Returns the current username
+     *
+     * @return
+     */
     public String getUsername() {
         return this.user.getUsername();
     }
@@ -165,25 +219,40 @@ public class IMClientController extends AbstractController {
     // Actions of the controller
     //
     // ------------------------------------------------------------
+    /**
+     * Singnin action
+     *
+     * @param user
+     * @param serverIp
+     */
     public void signin(User user, String serverIp) {
-        this.applicationStage = ApplicationStages.SIGNIN;
+        // Changing the state of the application
+        this.applicationStage = ApplicationStages.SIGNIN_IN_PROGRESS;
+        // Saving the server IP
         this.serverIp = serverIp;
+        // Starting the message listener
         this.messageListener = new MessageListener(this, user.getPort());
-
         this.messageListener.startListener();
+
+        // Sending the signin message
         this.sendObjectToServer(new SigninRequestMessage(user));
         this.view.setTitle("Polak IM - " + user.getUsername());
         this.user = user;
     }
 
+    /**
+     * Signout action
+     */
     public void signout() {
-        this.applicationStage = ApplicationStages.SIGNIN;
-        
-        if( this.messageListener != null )
-        {
+        // Changing the state of the application
+        this.applicationStage = ApplicationStages.SIGNIN_IN_PROGRESS;
+
+        // Stopping the message listener if it was previously defined
+        if (this.messageListener != null) {
             this.messageListener.stopListener();
         }
-        
+
+        // Sending signout request and then quiting
         try {
             (new SyncObjectSender(this.serverIp, Config.SERVER_LISTEN)).send(new SignoutRequestMessage(user.getUsername()));
         } catch (Exception e) {
@@ -191,12 +260,10 @@ public class IMClientController extends AbstractController {
         } finally {
             System.exit(0);
         }
-        
-        
     }
 
     /**
-     * Sends an object to the server
+     * Helper for sending an object to the server
      *
      * @param o
      * @return
@@ -205,38 +272,58 @@ public class IMClientController extends AbstractController {
         try {
             new AsyncObjectSender(o, this.serverIp, Config.SERVER_LISTEN, this, true);
         } catch (Exception e) {
-            e.printStackTrace();
+            //e.printStackTrace();
             return false;
         }
 
         return true;
     }
 
+    /**
+     * Helper for sending an object to the user
+     *
+     * @param username
+     * @param message
+     */
     public void sendMessageToUser(String username, String message) {
+
+        // Getting the username
         User user = this.userList.get(username);
 
+        // Wrong username
         if (user == null) {
             return;
         }
 
+        // Sending message in an asynchronous manner
         try {
             new AsyncObjectSender(new TextMessage(this.user.getUsername(), username, message), user.getIp().getHostAddress(), user.getPort(), this, false);
         } catch (Exception e) {
-            e.printStackTrace();
+            //e.printStackTrace();
         }
     }
 
+    /**
+     * Helper for sending an file to the user
+     *
+     * @param username
+     * @param file
+     */
     public void sendFileToUser(String username, File file) {
+
+        // Getting the username
         User user = this.userList.get(username);
 
+        // Wrong username
         if (user == null) {
             return;
         }
 
+        // Sending file in an asynchronous manner
         try {
             new AsyncFileSender(file, user.getIp().getHostAddress(), user.getPort(), new FileTransferDialog(file.getName(), file.length()));
         } catch (Exception e) {
-            e.printStackTrace();
+            //e.printStackTrace();
         }
     }
 
--- src/controller/IMServerController.java
+++ src/controller/IMServerController.java
@@ -28,10 +28,10 @@ public class IMServerController extends AbstractController {
     private ServerWindow view;
     private Timer pingerTimer;
 
-    // ------------------------------------------------------------
-    
     /**
-     * Singletone
+     * Singletone getInstance method
+     *
+     * @return
      */
     public static IMServerController getInstance() {
         if (IMServerController.instance == null) {
@@ -41,77 +41,97 @@ public class IMServerController extends AbstractController {
         return IMServerController.instance;
     }
 
-    // ------------------------------------------------------------
-    
     /**
-     * Default constructor
+     * Private controller for the singletone
      */
     protected IMServerController() {
         this.view = new ServerWindow(this);
 
+        // Logging
         this.println("Initializing IMServerController\n");
     }
 
-	// ------------------------------------------------------------
     /**
-     * Static method, starts the server
+     * The app starter method
      *
      * @param args
      */
     public static void main(String args[]) {
-        // Initialize Controller
+        // Initializes the instance of the server controller
         IMServerController.getInstance();
     }
 
-    // ------------------------------------------------------------
-    
     /**
      * Method for handeling incoming messages
      *
-     * @param mx
+     * @param m
+     * @param socket
      */
     public void handleIncomingMessage(Object m, Socket socket) {
+
+        // Logging
         this.println("Handeling incoming request");
 
+        // Checking the type of the message
         if (m instanceof SigninRequestMessage) {
+
+            // Logging
             this.println("Received SigninRequestMessage");
+            // Extracting the user
             User user = ((SigninRequestMessage) m).getUser();
 
-			// TODO Check if there is any other user from the same IP having the same port
+            // TODO Check if there is any other user from the same IP having the same port
+            // Checking whether the username is not yet taken
             if (this.userList.get(user.getUsername()) != null) {
-                new AsyncObjectSender(new ServerSignInFailureMessage(), socket, this, false); // Sending FAILURE message
+                // Sending FAILURE message
+                new AsyncObjectSender(new ServerSignInFailureMessage(), socket, this, false);
                 return;
             }
 
+            // Sets the incoming user IP address
             user.setInetAddress(socket.getInetAddress());
 
+            // Changing the userlist and triggering the pinger task
             synchronized (this.userList) {
                 this.userList.put(user.getUsername(), user);
                 PingerTask.markUserlistModified();
             }
 
-            new AsyncObjectSender(new ServerSignInSuccessMessage(), socket, this, false); // Sending OK message
+            // Sending login confirmation message
+            new AsyncObjectSender(new ServerSignInSuccessMessage(), socket, this, false);
+
         } else if (m instanceof SignoutRequestMessage) {
+
+            // Logging
             this.println("Received SignoutRequestMessage");
+
+            // Removing the user from the userlist
             this.userList.remove(((SignoutRequestMessage) m).getUsername());
+            // Triggering the pinger task
             PingerTask.markUserlistModified();
+
         } else if (m instanceof RefreshUserlistRequestMessage) {
+
+            // Logging
             this.println("Received RefreshUserlistRequestMessage");
+
+            // Sending back the latest version of the userlist
             new AsyncObjectSender(new RefreshUserlistResponseMessage(this.userList), socket, this, false); // Sending OK message
+
         } else {
-            this.println("Received invalid request");
+            // Logging
+            this.println("Wrong request, unknown message type");
         }
     }
 
-    // ------------------------------------------------------------
-    
     /**
      * Notify method is called from the ParallelObjectSender on failure
      */
     public void notify(Exception e) {
 
+        // TODO Make it deterministic
+        // Canceling the timer
         this.pingerTimer.cancel();
-
     }
 
     /**
@@ -123,6 +143,11 @@ public class IMServerController extends AbstractController {
         ServerWindow.logPrintln(line);
     }
 
+    /**
+     * Returns the userlist
+     *
+     * @return
+     */
     public UserList getUserList() {
         return this.userList;
     }
@@ -140,10 +165,14 @@ public class IMServerController extends AbstractController {
     public void start(int port) {
         // Initialize MessageListener
         this.messageListener = new MessageListener(this, port);
+        // Initialising the userlist
         this.userList = new UserList();
+        // Logging
         this.println("Starting server..");
+        // Starting message listener
         this.messageListener.startListener();
 
+        // Scheduling the pinger task
         this.pingerTimer = new Timer();
         PingerTask pTask = new PingerTask();
         this.pingerTimer.schedule(pTask, 1000, 10000);
@@ -154,7 +183,9 @@ public class IMServerController extends AbstractController {
      * Stopps the listener
      */
     public void stop() {
+        // Canceling the timer
         this.pingerTimer.cancel();
+        // Stopping the listener
         this.messageListener.stopListener();
     }
 
