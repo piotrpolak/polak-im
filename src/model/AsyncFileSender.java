5c53b811cdcb2dd8c290d19709e6bae754d37b45
Piotr Polak <pepis@closbrothers.pl>
2015-09-10 15:26:12 +0200

    Refactored AsyncFileSender

--- src/model/AsyncFileSender.java
+++ src/model/AsyncFileSender.java
@@ -13,18 +13,34 @@ import java.io.File;
 import javax.net.ssl.*;
 
 import controller.IMClientController;
-import view.client.FileTransferDialog;
 import message.FileTransferRequestMessage;
+import view.client.TransferNotifierInterface;
 
+/**
+ * Asynchronous file sender
+ *
+ * Enables sending incoming file transfers in a separate nonblocking thread
+ *
+ * @author Piotr Polak
+ *
+ */
 public class AsyncFileSender extends Thread {
 
     private File file = null;
     private Socket socket = null;
     private String ip = null;
     private int port = -1;
-    private FileTransferDialog notifier = null;
+    private TransferNotifierInterface notifier = null;
 
-    public AsyncFileSender(File file, String ip, int port, FileTransferDialog notifier) {
+    /**
+     * Default constructor
+     * 
+     * @param file
+     * @param ip
+     * @param port
+     * @param notifier 
+     */
+    public AsyncFileSender(File file, String ip, int port, TransferNotifierInterface notifier) {
         this.file = file;
         this.ip = ip;
         this.port = port;
@@ -36,9 +52,13 @@ public class AsyncFileSender extends Thread {
     /**
      * Thread main method
      */
+    @Override
     public void run() {
+        // Initializing socket only if it was not created before
         if (this.socket == null) {
+            // Checking ip and port
             if (this.ip != null && this.port > -1) {
+                // Initializing desired socket
                 try {
                     if (Config.USE_SSL) {
                         this.socket = SSLSocketFactory.getDefault().createSocket(this.ip, this.port);
@@ -48,60 +68,86 @@ public class AsyncFileSender extends Thread {
                         this.socket = new Socket(this.ip, this.port);
                     }
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    // Unable to connect
+                    // TODO set notification message
                     return;
                 }
             } else {
+                // Wrong IP of port
                 return;
             }
         }
+        
+        ObjectOutputStream out;
+        FileInputStream fis;
+        
+        // Sending the file transfer envelope
         try {
-            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+            out = new ObjectOutputStream(socket.getOutputStream());
             out.writeObject(new FileTransferRequestMessage(IMClientController.getInstance().getUsername(), this.file.getName(), this.file.length()));
-
-            FileInputStream file_input = null;
-            int buffer_read_n_bytes = 0;
-            long total_transfered = 0;
+            
+        } catch (IOException e) {
+            return;
+        }
+        
+        
+        try {
+            int bytesRead = 0;
+            long totalBytesTransfered = 0;
+            
+            // Creating the buffer with the length specified in the config
             byte[] buffer = new byte[generic.Config.FILE_TRANSFER_BUFFER_SIZE];
 
+            // Getting the socket output stream
             OutputStream outStream = socket.getOutputStream();
+            
             try {
 
-                file_input = new FileInputStream(file);
+                // Creating input for reading the file
+                fis = new FileInputStream(file);
 
                 try {
-
-                    while ((buffer_read_n_bytes = file_input.read(buffer)) != -1) { // While reading from file
-                        outStream.write(buffer, 0, buffer_read_n_bytes); // Writing to buffer
-
-                        outStream.flush(); // Flushing the buffer
-                        total_transfered += buffer_read_n_bytes;
-                        this.notifyNotifier(total_transfered);
+                    // Reading from file in a loop
+                    while ((bytesRead = fis.read(buffer)) != -1) {
+                        // Writing to buffer
+                        outStream.write(buffer, 0, bytesRead); 
+                        // Flushing the buffer
+                        outStream.flush();
+                        // Some statistics
+                        totalBytesTransfered += bytesRead;
+                        
+                        // Set notification
+                        this.notifyNotifier(totalBytesTransfered);
                     }
-                    outStream.flush();  // Flushing remaining buffer
+                    
+                    // Flushing remaining buffer, just in case
+                    outStream.flush();  
 
                 } catch (IOException e) {
+                    // TODO set notification message
                 }
 
                 try {
-                    file_input.close();
+                    fis.close();
                 } // Closing file input stream
                 catch (IOException e) {
+                    // TODO set notification message
                 }
 
             } catch (FileNotFoundException e) {
+                // TODO set notification message
             }
 
         } catch (IOException e) {
-            e.printStackTrace();
+            // TODO set notification message
         }
 
     }
 
     /**
-     * Notifies notifier, if specified
-     *
-     * @param e
+     * Notifies the notifier object about the number of bytes transfered
+     * 
+     * @param transfered 
      */
     private void notifyNotifier(long transfered) {
         if (this.notifier != null) {
